f41a1bf01ef9abb8e9fd3e50322ad47b5d6c403f
pascalvanluit <pjvanluit@gmail.com>
2020-02-21 16:07:04 +0100

    Update mi_cv and start on mi_cv_kfold

--- Master Thesis/mi_cv_kfold.R
+++ Master Thesis/mi_cv_kfold.R
@@ -1,3 +1,43 @@
 mi_cv_kfold <- function(baseline.model, data, k = 4){
   
+  # Saving the baseline.model as model:
+  model <- baseline.model
+  
+  # Setting up the dataset to be split into k folds:
+  
+    # Adding a column with assignment for each observation:
+    n_obs      <- nrow(data)
+    select_vec <- rep(1:k, length.out = n_obs)
+    data_split <- data %>% mutate(fold = sample(select_vec))
+    
+  # Fitting the data to a the different training sets:
+  for (i in 1:k) {
+    
+    # Splitting the data in train and validation set
+    train <- data_split %>% filter(fold != i)
+    valid <- data_split %>% filter(fold == i)
+    
+    # Fitting a model on the training set:
+    fit_train_i <- lavaan::cfa(model, train)
+    
+    # Fitting the model to the validation set:
+    fit_valid_i <- lavaan::cfa(model, valid)
+    
+    # Finding which modification to added based on the training portion:
+    MIs <- lavaan::modindices(fit_train_i)
+    
+      # Wrangling the MIs:
+      MIs <- MIs %>% arrange(-mi) %>% select(lhs, op, rhs, mi)
+    
+    # Specifying a modification to be added to the model:
+    mod <- paste(MIs[1, 1], MIs[1, 2], MIs[1, 3], sep = " ")
+    
+  }
+  
 }
+
+
+# Testing out the things I'm adding in the function above to check how and if they work:
+n_obs <- nrow(HolzingerSwineford1939)
+select_vec <- rep(1:4, length.out = n_obs)
+data_split <- HolzingerSwineford1939 %>% mutate(fold = sample(select_vec))
