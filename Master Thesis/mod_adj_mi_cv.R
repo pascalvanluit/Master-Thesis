917634ccdaa9de42be77a75565e968a9ee188f4d
pascalvanluit <pjvanluit@gmail.com>
2020-03-05 15:27:29 +0100

    Continuing with mod_adj_mi_cv

--- Master Thesis/mod_adj_mi_cv.R
+++ Master Thesis/mod_adj_mi_cv.R
@@ -32,19 +32,19 @@ mod_adj_mi_cv <- function(baseline.model, data, k = 5, min.mi = 10){
       fit_test  <- lavaan::cfa(model, test, start = fit_train, do.fit = FALSE)
     
       # Obtaining MI values:
-      mi.test <- lavaan::modindices(fit_test)
+      mi_test <- lavaan::modindices(fit_test)
     
         # Wrangling MI output:
-        mi.test <- mi.test %>% 
+        mi_test <- mi_test %>% 
           select(lhs, op, rhs, mi)
     
       # Combining the OOS MI values:
-      cv_mi[, 4] <- mi.test[, 4]
+      cv_mi[, 4] <- mi_test[, 4]
     
       # Fixing up the MI output
       cv_mi <- cv_mi %>%
         mutate_if(is.numeric, round, 3) %>% 
-        mutate(mi / k) %>% 
+        mutate(mi = mi / k) %>% 
         select(lhs, op, rhs, mi) %>% 
         arrange(-mi)
     
@@ -86,19 +86,19 @@ mod_adj_mi_cv <- function(baseline.model, data, k = 5, min.mi = 10){
       fit_test  <- lavaan::cfa(model, test, start = fit_train, do.fit = FALSE)
     
       # Obtaining MI values:
-      mi.test <- lavaan::modindices(fit_test)
+      mi_test <- lavaan::modindices(fit_test)
     
       # Wrangling MI output:
-      mi.test <- mi.test %>% 
+      mi_test <- mi_test %>% 
         select(lhs, op, rhs, mi)
     
       # Combining the OOS MI values:
-      cv_mi[, 4] <- mi.test[, 4]
+      cv_mi[, 4] <- mi_test[, 4]
     
       # Fixing up the MI output
       cv_mi <- cv_mi %>%
         mutate_if(is.numeric, round, 3) %>% 
-        mutate(mi / k) %>% 
+        mutate(mi = mi / k) %>% 
         select(lhs, op, rhs, mi) %>% 
         arrange(-mi)
       
@@ -115,122 +115,3 @@ mod_adj_mi_cv <- function(baseline.model, data, k = 5, min.mi = 10){
   
 }
 
-
-mi_cv_kfold <- function(baseline.model, data, k = 5){
-  
-  # Saving the baseline.model as model:
-  model <- baseline.model
-  
-  ## Setting up the dataset to be split into k folds:
-  
-  # Adding a column with assignment for each observation:
-  n.obs      <- nrow(data)
-  select.vec <- rep(1:k, length.out = n.obs)
-  data.split <- data %>% mutate(fold = sample(select.vec))
-  
-  # Create output vector for in the validation set:
-  fit.valids <- rep(0, k)
-  
-  ############
-  # FOR LOOP #
-  ############
-  
-  ## Fitting the data to a the different training sets:
-  for (i in 1:k) {
-    
-    # Splitting the data in train and validation set
-    train <- data.split %>% filter(fold != i)
-    valid <- data.split %>% filter(fold == i)
-    
-    # Fitting the baseline model on the training set:
-    fit.train.i <- lavaan::cfa(model, train)
-    
-    # Finding which modification to add based on the training portion:
-    MIs <- lavaan::modindices(fit.train.i)
-    
-    # Wrangling the MIs:
-    MIs <- MIs %>% arrange(-mi) %>% select(lhs, op, rhs, mi)
-    
-    # Specifying a modification to be added to the model:
-    mod <- paste(MIs[1, 1], MIs[1, 2], MIs[1, 3], sep = " ")
-    
-    # Fitting the baseline model to the validation set:
-    fit.valid.1.i <- lavaan::cfa(model, valid)
-    
-    # Adding a modification to the model:
-    mod.model <- paste(model, mod, sep = "\n")
-    
-    # Find the fit of the modified model on the validation set:
-    fit.valid.2.i <- lavaan::cfa(mod.model, valid)
-    
-    # Seeing whether this improves the fit:
-    chisq.diff.i <- lavaan::lavTestLRT(fit.valid.1.i, fit.valid.2.i)
-    
-    # Assigning whether fit is significantly better or not:
-    ifelse(chisq.diff.i$`Pr(>Chisq)`[2] < .05, sign <- TRUE, sign <- FALSE)
-    
-    # Finding the mean p-value:
-    mean.chisq.diff <- mean(chisq.diff.i)
-    
-    # Creating chisq.diff object:
-    # mean.chisq.diff <- list(`Pr(>Chisq)` = 0)
-    
-    ###################
-    # WHILE FUNCTTION #
-    ###################
-    while (mean.chisq.diff$`Pr(>Chisq)` < .05) {
-      
-      # Extracting a modification to be added to the model:
-      mod <- paste(MIs[1, 1], MIs[1, 2], MIs[1, 3], sep = " ")
-      
-      # Adding a modification to the model:
-      model <- paste(model, mod, sep = "\n")
-      
-      # Fitting the model to the validation set:
-      fit.valid.2.i <- lavaan::cfa(model, valid)
-      
-      # Obtaining chisq.diffs and finding the mean:
-      fit.valids[i] <- lavaan::lavTestLRT(fit.valid.1.i, fit.valid.2.i)
-      mean.chisq.diff <- mean(fit.valids[i])
-      
-      # Fitting the model to the training set:
-      fit.train <- lavaan::cfa(model, train)
-      
-      # Obtaining new MI values:
-      MIs <- lavaan::modindices(fit.train)
-      
-      # Wrangling the MIs:
-      MIs <- MIs %>% arrange(-mi) %>% select(lhs, op, rhs, mi)
-      
-      # Print the final model:
-      print(model)
-      
-    } 
-    
-    
-    # # Adding the modification to the model:
-    # model <- paste(model, mod, sep = "\n")
-    # 
-    # # Fitting the modified model on the valid set:
-    # fit_valid_2 <- lavaan::cfa(model, valid)
-    # 
-    # # Checking the chisq.diff in the validation set:
-    # chisq.diff[i] <- lavaan::lavTestLRT(fit_valid_1_i, fit_valid_2)
-    # 
-    # # Getting a mean of the chisq.diff's
-    # mean.chisq.diff <- mean(chisq.diff[i])
-    
-    # # Calculate the model fit in the validation set:
-    # fit_measures_valids <- lavaan::fitmeasures(fit_valid, c("chisq", "df", "pvalue"))
-    # 
-    # # Find the average of the fits in the validation sets:
-    # fit_valids[i] <- mean(fit_measures_valids)
-    # 
-    # #Print the final model:
-    # print(model)
-    
-  }
-  
-  # return()
-  
-}
