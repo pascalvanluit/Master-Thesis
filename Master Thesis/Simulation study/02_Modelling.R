57f0c8650f5710ad5c3e541caad0fa1faf00c974
pascalvanluit <pjvanluit@gmail.com>
2020-03-31 10:35:37 +0200

    Update simulation study

--- Master Thesis/Simulation study/02_Modelling.R
+++ Master Thesis/Simulation study/02_Modelling.R
@@ -21,12 +21,23 @@ source("Methods/mod_adj_chisq_cv.R")
 ##############
 # mod_adj_mi #
 ##############
-lapply(sim_data, mod_adj_mi, baseline.model = model, min.mi = 10)
+
+# Applying mod_adj_mi to each simulated dataset:
+models_mod_adj_mi <- lapply(sim_data, mod_adj_mi, baseline.model = model, min.mi = 10)
+
+# Creating a fit object for each model:
+fits_mod_adj_mi <- lapply(models_mod_adj_mi, lavaan::cfa)
+
+# Obtaining the summary of each fit:
+sums_mod_adj_mi <- lapply(fits_mod_adj_mi, summary)
+
+# Obtaining the parameter of interest from each fit summary:
+pois_mod_adj_mi <- subset(sums_mod_adj_mi$PE$est, sums_mod_adj_mi$PE$lhs == "f1" & sums_mod_adj_mi$PE$rhs == "f2")
+
+# Finding the MSE of the PoI:
+mse_mod_adj_mi <- pois_mod_adj_mi - conditions
 
 
-models <- vector("list", length(sim_data))
-
-models[[9]]
 
 for (i in length(sim_data)) {
   
@@ -66,10 +77,20 @@ modelling_mod_adj_mi <- function(model = model, min.mi = 10){
 #################
 # mod_adj_mi_cv #
 #################
-mod_adj_mi_cv()
+
+# Applying mod_adj_mi_cv to each simulated dataset:
+models_mod_adj_mi_cv <- lapply(sim_data, mod_adj_mi_cv, baseline.model = model, min.mi = 10)
+
+# Creating a fit object for each model:
+fits_mod_adj_mi_cv <- lapply(models_mod_adj_mi_cv, lavaan::cfa)
 
 
 ####################
 # mod_adj_chisq_cv #
 ####################
-mod_adj_chisq_cv()
+
+# Applying mod_adj_mi to each simulated dataset:
+models_mod_adj_chisq_cv <- lapply(sim_data, mod_adj_chisq_cv, baseline.model = model, min.mi = 10)
+
+# Creating a fit object for each model:
+fits_mod_adj_chisq_cv <- lapply(models_mod_adj_chisq_cv, lavaan::cfa)
