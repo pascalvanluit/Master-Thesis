b6e01e46310f8859fe4340cdf3d0f7fb35b94b03
pascalvanluit <pjvanluit@gmail.com>
2020-04-12 13:03:47 +0200

    Update simulation study

--- Master Thesis/Simulation study/05_Run.R
+++ Master Thesis/Simulation study/05_Run.R
@@ -1,5 +1,12 @@
+start.time <- Sys.time()
+
 source("Simulation study/00_create_conditions_grid.R")
 source("Simulation study/01_simulation.R")
 source("Simulation study/02_Modelling.R")
 source("Simulation study/03_Analysis.R")
 source("Simulation study/04_Plots.R")
+
+end.time <- Sys.time()
+time.taken <- end.time - start.time
+
+time.taken
