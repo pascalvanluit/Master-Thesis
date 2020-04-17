3238a14fea7c56fa908df3b7994475dee85c3556
pascalvanluit <pjvanluit@gmail.com>
2020-04-17 10:54:44 +0200

    Update simulation study

--- Master Thesis/Simulation study/02b_Modelling.R
+++ Master Thesis/Simulation study/02b_Modelling.R
@@ -1,12 +1,7 @@
 source("Simulation study/poi.R")
-models_and_fits <- read_rds(path = "Simulation study/02_models_and_fits.rds")
 
-models <- models_and_fits %>% 
-  select(contains("models"))
-
-fits <- models_and_fits %>% 
-  select(contains("fits"))
-
+library(gmodels)
+library(shapes)
             ########################################################
             # Obtaining the estimates of the Parameter of Interest # 
             ########################################################
@@ -21,78 +16,26 @@ conditions$models <- lapply(conditions$outputs, lapply, function(x) list.extract
 # Using a nested apply to obtain fits:
 conditions$fits <- lapply(conditions$outputs, lapply, function(x) list.extract(x, 'fit'))
 
+
 # Using a nested apply to obtain poi estimates:
-conditions$fits <- lapply(conditions$outputs, lapply, function(x) list.extract(x, 'fit'))
+conditions$pois <- lapply(conditions$pois, lapply, function(x) poi(x))
+
+# Using a nested apply to obtain mse of poi estimates:
+conditions$mses <- lapply(conditions$mses, lapply, function(x) (as.matrix(x) - conditions[,2]))
+
+# Using a nested apply to obtain mean mse and CI's for each condition:
+conditions$mean_mse <- lapply(conditions$mses, function(x) mean(unlist(x)))
+conditions$mse_ci_lower <- lapply(conditions$mses, function(x) ci(unlist(x))["CI lower"])
+conditions$mse_ci_upper <- lapply(conditions$mses, function(x) ci(unlist(x))["CI upper"])
 
 
+# Using nested apply to obtain estimated covariance matrices:
+conditions$covmats <- lapply(conditions$fits, lapply, fitted)
 
-# Getting all models of mod_adj_mi_4
-conditions$models <- vector("list", nrow(conditions))
+# Using nested lapply to obtain distcov from true covmat:
+conditions$truecov <- purrrlyr::by_row(conditions, create_true_covmat)
 
-for (i in nrow(conditions)) {
-  
-  for (j in 1:replications) {
-    
-    modellist <- lapply(conditions$outputs[[i]], function(x) list.extract(x, 'model'))
-    conditions$models[j] <- list(modellist)
-  }
-  
-}
 
-# Getting all fits of mod_adj_mi_4
-conditions$fits <- vector("list", nrow(conditions))
-
-for (i in nrow(conditions)) {
-  
-  for (j in 1:replications) {
-    
-    fitlist <- lapply(conditions$outputs[[i]], function(x) list.extract(x, 'fit'))
-    conditions$fits[j] <- list(fitlist)
-  }
-  
-}
-
-# Getting all poi values of mod_adj_mi_4
-conditions$poi <- vector("list", nrow(conditions))
-
-for (i in nrow(conditions)) {
-  
-  for (j in 1:replications) {
-    
-    poilist <- lapply(conditions$fits[[i]], poi)
-    
-    conditions$poi[j] <- list(poilist)
-  }
-  
-}
- 
-# Getting the mse value for each estimate of poi:
-conditions$mses <- vector("list", nrow(conditions))
-
-for (i in nrow(conditions)) {
-  
-  for (j in 1:replications) {
-    
-    mseslist <- lapply(conditions$poi[[i]], function(x) ((unlist(x) - conditions[i, 2])^2))
-    
-    conditions$mses[j] <- list(mseslist)
-  }
-  
-}
-
-# Getting the mean mse of each condition:
-conditions$mean_mse <- vector("list", nrow(conditions))
-
-for (i in nrow(conditions)) {
-  
-  # for (j in 1:replications) {
-    
-    mean_mse <- lapply(conditions$mses[i], function(x) mean(unlist(x)))
-    
-    conditions$mean_mse[i] <- mean_mse
-  # }
-  
-}
 
 
 
@@ -114,9 +57,6 @@ pois_mod_adj_mi_4 <- lapply(unname(pois_mod_adj_mi_4), poi)
 
 # Computing the MSE of the parameter of interest:
 mse_mod_adj_mi_4 <- as.data.frame(c((unlist(pois_mod_adj_mi_4) - conditions[, 2])^2))
-
-
-
 
 
 
